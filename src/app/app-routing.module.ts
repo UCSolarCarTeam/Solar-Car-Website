8a0a96e47107b50b9fd63a458d8439987bdac1ce
Thomas Vy <vythomas97@gmail.com>
2019-11-27 20:44:09 -0700

    Add skeleton html for Support Page
    Added the skeleton html page for the Support Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SupportUsMainComponent } from './components/support-us-page/support-us-main/support-us-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'support-us', component: SupportUsMainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SponsorComponent } from './components/support-us-page/sponsor/sponsor.component';
+import { DonateComponent } from './components/support-us-page/donate/donate.component';
+import { InKindDonationsComponent } from './components/support-us-page/in-kind-donations/in-kind-donations.component';
+import { SocialMediaComponent } from './components/common/social-media/social-media.component';
+import { SupportUsMainComponent } from './components/support-us-page/support-us-main/support-us-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SupportUsMainComponent,
+    SponsorComponent,
+    DonateComponent,
+    InKindDonationsComponent,
+    SocialMediaComponent,
+    SupportUsMainComponent,
   ],
   imports: [
     BrowserModule,

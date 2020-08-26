1ff9ef20d60d69d725d3bc26942028234c09ec64
Bill Luu <billluu619@gmail.com>
2020-08-25 21:26:17 -0600

    SFT-185 Create Member Service and add members.json
    Made a member service that will read members.json to get all the people
    on the team.
    Members can be updated by editing a google doc with the same style as
    here: https://docs.google.com/spreadsheets/d/1FRJIE2hNy23xtDKZlXi4ocX0u5LZ5zQf9NmUuJ1h0mg/edit?usp=sharing
    
    Fill that in, download it as csv, and feed it through an online csv-json
    tool to get what can be used as members.json

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from './member.model';
+import { MembersService } from '../services/members.service';
 
 @Component({
   selector: 'app-team',
@@ -10,34 +11,15 @@ export class TeamComponent implements OnInit {
   teamCaptain: Member;
   enggManager: Member;
   businessManager: Member;
+  members: Member[];
 
-  constructor() { }
+  constructor(private m: MembersService) { }
 
   ngOnInit() {
-    this.teamCaptain = ({
-      name: 'Alejandro A. Garza',
-      position: 'Team Captain',
-      major: 'Electrical Engineering - Year 4',
-      description: `I joined the team because I have always been passionate about
-      clean energy and electric cars. Also I get to meet great people that share the same interests as me.
-       Fun Fact: I love Mexican food.`,
-      image: 'Alejandro-Garza.jpg',
-    });
-    this.enggManager = ({
-      name: 'Liam Stoddard',
-      position: 'Engineering Manager',
-      major: 'Electrical Engineering - Year 5',
-      description: `To apply my knowledge, learn new skills, and work with new technology.
-      Fun Fact: I piloted a helicopter once. It was terrifying.`,
-      image: 'Liam-Stoddard.jpg'
-    });
-    this.businessManager = ({
-      name: 'Joseph Stensland',
-      position: 'Business Manager',
-      major: '?',
-      description: '?',
-      image: '?'
-    });
+    this.teamCaptain = this.m.TeamCaptain();
+    this.enggManager = this.m.EngineeringManager();
+    this.businessManager = this.m.BusinessManager();
+    this.members = this.m.AllMembers();
   }
 
 }

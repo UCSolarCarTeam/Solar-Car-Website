c1757c9c2a6497964713fd844dd4a7f57d2dd30b
JensVarughese <jensvarughese@gmail.com>
2021-01-17 12:29:30 -0700

    SFT-133 Finish Elysia Page

--- src/app/elysia/elysia.component.ts
+++ src/app/elysia/elysia.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElysiaComponent implements OnInit {
 
-  constructor() { }
+  galleryPics: string[];
+
+  specs: string[][];
+  specs1: string[][];
+  specs2: string[][];
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+
+    this.galleryPics = ['assets/elysia/gallery/img1.jpg',
+                        'assets/elysia/gallery/img2.jpg',
+                        'assets/elysia/gallery/img3.jpg',
+                        'assets/elysia/gallery/img4.jpg',
+                        'assets/elysia/gallery/img5.jpg',
+                        'assets/elysia/gallery/img6.jpg',
+                        'assets/elysia/gallery/img7.jpg',
+                        'assets/elysia/gallery/img8.jpg'
+                       ];
+
+    this.specs = [['Year Built','2016/2019'],
+                  ['Body Style', 'Two-door coupe with large rear hatch'],
+                  ['Seating', 'Two front'],
+                  ['Motor', 'Two rear in-wheel direct drive motors'],
+                  ['Rear Drive Motor Capacity', '14 Horsepower peak per motor'],
+                  ['Time to Design', '8 months'],
+                  ['Mold Construction', '3 months'],
+                  ['Car Construction', '3 months'],
+                  ['Weight', '545 kg (1200 lbs)'],
+                  ['Length', '4.5 metres (15 ft)'],
+                  ['Height', '1.2 metres (4 ft)'],
+                  ['Width', '1.8 metres (6 ft)'],
+                  ['Track', '1.4 metres (4.6 ft)'],
+                  ['Base', '2.7 metres (8.9 ft)'],
+                  ['Array Power', '1000-1200 Watts'],
+                  ['Solar Cells', '318 Mono-Crystalline Silicon Cells'],
+                  ['MPPT (Maximum Power Point Tracker)', '2 Two-Channel MPPTs by Dilithium Power Systems'],
+                  ['Batteries', '1353 Li-Ion 1440 cells (244.4 lbs)'],
+                  ['Battery Box', 'Innegra/ Basalt Composite'],
+                  ['Instrumentation and Communications', 'Driver display in steering wheel, tablet interface in dash, wifi link to chase vehicle'],
+                  ['Suspension', 'Double A-Arm, aluminum and steel'],
+                  ['Top Shell', 'Carbon Fibre Reinforced EPS'],
+                  ['Bottom Shell', 'Carbon Fibre Reinforced Corecell, EPS Mix'],
+                 ];
+    // split the array into 2
+    this.specs1 = this.specs.slice( 0, this.specs.length / 2 );
+    this.specs2 = this.specs.slice( this.specs.length / 2, this.specs.length );
   }
 
 }

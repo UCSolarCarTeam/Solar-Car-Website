cac0b6c7cf7f6e1feb35f0ee07cec5c95aa4eadb
Jayden Bischoff <jayden.bischoff1@ucalgary.ca>
2021-03-13 12:21:36 -0700

    fixed header by implementing a bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { SocialMediaComponent } from './common/social-media/social-media.compone
 import { CarsInfoComponent } from './cars/cars-info/cars-info.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderLinksComponent } from './header/header-links/header-links.component';
-import { HeaderHambugerMenuComponent } from './header/header-hambuger-menu/header-hambuger-menu.component';
 import { CaptainComponent } from './team/captain/captain.component';
 import { MembersComponent } from './team/members/members.component';
 import { GalleryItemComponent } from './team/members/gallery-item/gallery-item.component';
@@ -47,7 +46,6 @@ import { FooterComponent } from './footer/footer.component';
     CarsInfoComponent,
     HeaderComponent,
     HeaderLinksComponent,
-    HeaderHambugerMenuComponent,
     CaptainComponent,
     MembersComponent,
     GalleryItemComponent,

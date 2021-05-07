ef54edb59e6cc46e70668b8001250345ccaef72f
Liam Conway <11491642+liconway@users.noreply.github.com>
2021-05-06 19:17:44 -0600

    Create Portal feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
+  { path: 'portal', loadChildren: () => import('./portal/portal.module').then(m => m.PortalModule) },
   { path: '', loadChildren: () => import('./business/business.module').then(m => m.BusinessModule) },
 ];
 

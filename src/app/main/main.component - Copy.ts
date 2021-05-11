4d4f06d99f2b6a8789b99ef6c21d93f0eaa8d3fc
JessieG123 <jessieguo863@gmail.com>
2021-05-10 20:28:42 -0600

    created copies of main component for backup

---
+++ src/app/main/main.component - Copy.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd) ) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
+}

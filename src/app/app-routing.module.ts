4aee7863c42a19ffe40c4fefdfaad2892eca1574
JensVarughese <jensvarughese@gmail.com>
2021-05-22 13:20:11 -0600

    Fix errors from merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarsComponent } from './cars/cars.component';
-import { SupportUsComponent } from './support-us/support-us.component';
-import { SponsorsComponent } from './sponsors/sponsors.component';
-import { NewsComponent } from './news/news.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { TeamComponent } from './team/team.component';
+import { CarsComponent } from './business/cars/cars.component';
+import { SupportUsComponent } from './business/support-us/support-us.component';
+import { SponsorsComponent } from './business/sponsors/sponsors.component';
+import { NewsComponent } from './business/news/news.component';
+import { ContactUsComponent } from './business/contact-us/contact-us.component';
+import { TeamComponent } from './business/team/team.component';
 import { MainComponent } from './business/main/main.component';
-import { ArticleComponent } from './news/article/article.component';
-import { ElysiaComponent } from './elysia/elysia.component';
+import { ArticleComponent } from './business/news/article/article.component';
+import { ElysiaComponent } from './business/elysia/elysia.component';
 
 const routes: Routes = [
   { path: 'portal', loadChildren: () => import('./portal/portal.module').then(m => m.PortalModule) },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { SupportUsComponent } from './support-us/support-us.component';
-import { SponsorsComponent } from './sponsors/sponsors.component';
-import { NewsComponent } from './news/news.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { TeamComponent } from './team/team.component';
-import { SocialMediaComponent } from './common/social-media/social-media.component';
-import { CarsInfoComponent } from './cars/cars-info/cars-info.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderLinksComponent } from './header/header-links/header-links.component';
-import { CaptainComponent } from './team/captain/captain.component';
-import { MembersComponent } from './team/members/members.component';
-import { GalleryItemComponent } from './team/members/gallery-item/gallery-item.component';
+import { CarsComponent } from './business/cars/cars.component';
+import { SupportUsComponent } from './business/support-us/support-us.component';
+import { SponsorsComponent } from './business/sponsors/sponsors.component';
+import { NewsComponent } from './business/news/news.component';
+import { ContactUsComponent } from './business/contact-us/contact-us.component';
+import { TeamComponent } from './business/team/team.component';
+import { SocialMediaComponent } from './business/common/social-media/social-media.component';
+import { CarsInfoComponent } from './business/cars/cars-info/cars-info.component';
+import { HeaderComponent } from './business/header/header.component';
+import { HeaderLinksComponent } from './business/header/header-links/header-links.component';
+import { CaptainComponent } from './business/team/captain/captain.component';
+import { MembersComponent } from './business/team/members/members.component';
+import { GalleryItemComponent } from './business/team/members/gallery-item/gallery-item.component';
 import { MainComponent } from './business/main/main.component';
-import { PostComponent } from './news/post/post.component';
-import { ArticleComponent } from './news/article/article.component';
-import { ElysiaComponent } from './elysia/elysia.component';
-import { FooterComponent } from './footer/footer.component';
+import { PostComponent } from './business/news/post/post.component';
+import { ArticleComponent } from './business/news/article/article.component';
+import { ElysiaComponent } from './business/elysia/elysia.component';
+import { FooterComponent } from './business/footer/footer.component';
 
 @NgModule({
   declarations: [

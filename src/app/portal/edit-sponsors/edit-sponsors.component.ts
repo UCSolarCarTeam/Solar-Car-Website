c4965ea2a84801af090787b441a81ef8a4c53668
Dillon <dillonapple7h@gmail.com>
2021-07-24 13:58:25 -0600

    Fix merge errors

--- src/app/portal/edit-sponsors/edit-sponsors.component.ts
+++ src/app/portal/edit-sponsors/edit-sponsors.component.ts
@@ -2,12 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { SponsorService } from 'src/app/services/sponsor.service';
 import { Sponsor } from 'src/app/models/sponsor';
-<<<<<<< HEAD
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { FileUploadService } from 'src/app/services/file-upload.service';
-=======
 import { FormBuilder, FormGroup } from '@angular/forms';
->>>>>>> 01cee6a (Fix linting errors)
 
 @Component({
   selector: 'app-edit-sponsors',
@@ -17,9 +12,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class EditSponsorsComponent implements OnInit {
   addSponsorForm: FormGroup;
   sponsors: Sponsor[];
-  filePath: string;
 
-  constructor(private sponsorService: SponsorService, private uploadService: FileUploadService, private formBuilder: FormBuilder) {
+  constructor(private sponsorService: SponsorService, private formBuilder: FormBuilder) {
     this.addSponsorForm = this.formBuilder.group({
       name: [''],
       link: [''],
@@ -39,19 +33,8 @@ export class EditSponsorsComponent implements OnInit {
     });
   }
 
-  async uploadLogo(event) {
-    const file = event.target.files[0];
-    this.filePath = await this.uploadService.uploadFile(file);
-  }
-
-  async addSponsor() {
-    let sponsor = { id: '',
-                    name: this.addSponsorForm.get('name').value,
-                    link: this.addSponsorForm.get('link').value,
-                    logo: null,
-                    logoUrl: this.filePath,
-                    tier: this.addSponsorForm.get('tier').value};
-    this.sponsorService.addSponsor(sponsor);
+  addSponsor() {
+    this.sponsorService.addSponsor(this.addSponsorForm.value);
   }
 
   deleteSponsor(id) {

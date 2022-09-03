24b96378dd3a8cdac75b4c4efd884d359b015abe
Jayden Bischoff <jayden.bischoff1@ucalgary.ca>
2022-09-02 22:17:24 -0600

    Updated recruitment page for 2022

--- src/app/business/recruitment/recruitment.component.ts
+++ src/app/business/recruitment/recruitment.component.ts
@@ -15,32 +15,32 @@ export class RecruitmentComponent implements OnInit {
       {
         name: 'Software', description: 'Our software team is responsible for programming the onboard embedded and visual communication \
         systems, and telemetry system. They also oversee our website.',
-        applicationForm: 'https://forms.gle/AMbysu6xwrVZr7BR7'
+        applicationForm: 'https://forms.gle/o4EFbPtG3WAej9Uy8'
       },
       {
         name: 'Mechanical', description: 'Our mechanical team is responsible for designing and building the car’s body and chassis, \
         suspension system, roll cage, and integrating electrical components into the car.',
-        applicationForm: 'https://forms.gle/xVVQ7J5zCkigwkTr8'
+        applicationForm: ' https://forms.gle/D6L7MeZPgKsyrAq5A'
       },
       {
         name: 'Electrical', description: 'Our electrical team designs, implements, and oversees all of Solar Car’s electrical components, \
          including the solar panels, batteries, motors, PCBs, and lights.',
-        applicationForm: 'https://forms.gle/FN8txxf7wYA4qVgAA'
+        applicationForm: 'https://forms.gle/aQGemvLzV8rGVo8Y6'
       },
       {
         name: 'Sponsorship', description: 'Our sponsorship team helps procure funding for the team by establishing and maintaining \
         relationships with sponsors and prospective donors.',
-        applicationForm: 'https://docs.google.com/forms/d/e/1FAIpQLScxYcxYzDGfnl-WEicDWXNdoCzhTSEhWbtyUdoon66vzfWozA/viewform'
+        applicationForm: 'https://forms.gle/FiXXdsBTwVNVRnye7'
       },
       {
         name: 'Accounting', description: 'Our accounting team keeps track of our organization\'s finances by managing financial assets, \
         creating budget projections, and maintaining the team\'s accounting books.',
-        applicationForm: 'https://forms.gle/daZwsGVLfTYMJ3Mo9'
+        applicationForm: 'https://forms.gle/DUWJkBzMk8EhKz329'
       },
       {
         name: 'Communications', description: 'Our communications team is responsible for the team’s marketing, organizing events, and \
         managing public relations.',
-        applicationForm: 'https://forms.gle/U7WDuCZJSRP1ArQ87'
+        applicationForm: 'https://forms.gle/uL6MVVi3jcSFuHdJ8'
       },
       {
         name: 'Multi-team', description: 'Our multi-team is a program for first-year students to gain introductory experience with both \

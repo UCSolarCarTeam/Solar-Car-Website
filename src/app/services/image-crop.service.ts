6dc587928959f0e63c0ea75172a89d0f74205c5f
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-13 14:31:15 -0600

    init crop service

---
+++ src/app/services/image-crop.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, NgModule } from '@angular/core';
+import { ImageCropperModule } from 'ngx-image-cropper';
+
+@NgModule({
+    imports: [
+        ...
+        ImageCropperModule
+    ],
+    declarations: [
+        ...
+    ],
+    exports: [
+        ...
+    ],
+    providers: [
+        ...
+    ]
+})
+
+export class ImageCropService {
+}

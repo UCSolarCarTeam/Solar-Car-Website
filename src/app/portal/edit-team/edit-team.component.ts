766a841556e3b3a55e7665ddb031d1d437d41d77
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-05-20 15:18:54 -0600

    add cropping preview

--- src/app/portal/edit-team/edit-team.component.ts
+++ src/app/portal/edit-team/edit-team.component.ts
@@ -6,6 +6,8 @@ import { FileUploadService } from 'src/app/services/file-upload.service';
 import { FileDeleteService } from 'src/app/services/file-delete.service';
 import { MemberService } from 'src/app/services/member.service';
 import { UserActionService } from 'src/app/services/user-action.service';
+import { ImageCroppedEvent, ImageTransform, Dimensions } from 'ngx-image-cropper';
+import { base64ToFile } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-edit-team',
@@ -23,6 +25,105 @@ export class EditTeamComponent implements OnInit {
   subteams: string[];
   actionHistory: UserAction[];
   deleteFlag: string;
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  canvasRotation = 0;
+  rotation = 0;
+  scale = 1;
+  showCropper = false;
+  containWithinAspectRatio = false;
+  transform: ImageTransform = {};
+
+  fileChangeEvent(event: any): void {
+      this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+      this.croppedImage = event.base64;
+      console.log(event, base64ToFile(event.base64));
+  }
+
+  imageLoaded() {
+      this.showCropper = true;
+      console.log('Image loaded');
+  }
+
+  cropperReady(sourceImageDimensions: Dimensions) {
+      console.log('Cropper ready', sourceImageDimensions);
+  }
+
+  loadImageFailed() {
+      console.log('Load failed');
+  }
+
+  rotateLeft() {
+      this.canvasRotation--;
+      this.flipAfterRotate();
+  }
+
+  rotateRight() {
+      this.canvasRotation++;
+      this.flipAfterRotate();
+  }
+
+  private flipAfterRotate() {
+      const flippedH = this.transform.flipH;
+      const flippedV = this.transform.flipV;
+      this.transform = {
+          ...this.transform,
+          flipH: flippedV,
+          flipV: flippedH
+      };
+  }
+
+
+  flipHorizontal() {
+      this.transform = {
+          ...this.transform,
+          flipH: !this.transform.flipH
+      };
+  }
+
+  flipVertical() {
+      this.transform = {
+          ...this.transform,
+          flipV: !this.transform.flipV
+      };
+  }
+
+  resetImage() {
+      this.scale = 1;
+      this.rotation = 0;
+      this.canvasRotation = 0;
+      this.transform = {};
+  }
+
+  zoomOut() {
+      this.scale -= .1;
+      this.transform = {
+          ...this.transform,
+          scale: this.scale
+      };
+  }
+
+  zoomIn() {
+      this.scale += .1;
+      this.transform = {
+          ...this.transform,
+          scale: this.scale
+      };
+  }
+
+  toggleContainWithinAspectRatio() {
+      this.containWithinAspectRatio = !this.containWithinAspectRatio;
+  }
+
+  updateRotation() {
+      this.transform = {
+          ...this.transform,
+          rotate: this.rotation
+      };
+  }
 
   constructor(private memberService: MemberService, private formBuilder: UntypedFormBuilder, private uploadService: FileUploadService,
               private userActionService: UserActionService, private deleteService: FileDeleteService) {

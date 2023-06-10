8e6c3f6fc3123e863a05cec10dd9bc41bafccee3
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2023-06-10 13:34:34 -0600

    TEL-50-Copy-over-image-cropping-logic-to-SPonsors-and-News (#133)
    * prettier formatting
    
    * sponsors cropper
    
    * prettify news component
    
    * add new component crop
    
    * only accept image files

--- src/app/portal/edit-news/edit-news.component.ts
+++ src/app/portal/edit-news/edit-news.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { NewsService } from 'src/app/services/news.service';
-import { News } from 'src/app/models/news';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
-import { FileUploadService } from 'src/app/services/file-upload.service';
+import { Component, OnInit, VERSION } from "@angular/core";
+import { NewsService } from "src/app/services/news.service";
+import { News } from "src/app/models/news";
+import { UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
+import { FileUploadService } from "src/app/services/file-upload.service";
+import { ImageCroppedEvent, LoadedImage } from "ngx-image-cropper";
 
 @Component({
-  selector: 'app-edit-news',
-  templateUrl: './edit-news.component.html',
-  styleUrls: ['./edit-news.component.css']
+  selector: "app-edit-news",
+  templateUrl: "./edit-news.component.html",
+  styleUrls: ["./edit-news.component.css"],
 })
 export class EditNewsComponent implements OnInit {
-
   addNewsForm: UntypedFormGroup;
   newsArticles: News[];
   thumbnail: File;
@@ -20,36 +20,57 @@ export class EditNewsComponent implements OnInit {
   updateNewsId: string;
   mainButtonText: string;
   isLinkOnlyFormat: boolean;
+  imageChangedEvent: any = "";
 
-  constructor(private newsService: NewsService, private uploadService: FileUploadService, private formBuilder: UntypedFormBuilder) {
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+    this.thumbnail = event.target.files[0];
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.previewThumbnailUrl = event.base64;
+    this.dataUrlToFile();
+  }
+
+  // Helper function
+  async dataUrlToFile(): Promise<void> {
+    const dataUrl = this.previewThumbnailUrl;
+    const fileName = this.thumbnail.name;
+    const res: Response = await fetch(dataUrl);
+    const blob: Blob = await res.blob();
+    const myImage = new File([blob], fileName, { type: "image/webp" });
+    this.thumbnail = myImage;
+  }
+
+  constructor(
+    private newsService: NewsService,
+    private uploadService: FileUploadService,
+    private formBuilder: UntypedFormBuilder
+  ) {
     this.addNewsForm = this.formBuilder.group({
-      name: [''],
-      date: [''],
-      thumnail: ['']
+      name: [""],
+      date: [""],
+      thumnail: [""],
     });
-    this.mainButtonText = 'Add News Article';
-    this.markdown =
-`Title
+    this.mainButtonText = "Add News Article";
+    this.markdown = `Title
 ---
 ---
 Something...
-<img src="https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png">`
-    ;
-    this.link = '';
+<img src="https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png">`;
+    this.link = "";
     this.isLinkOnlyFormat = false;
   }
 
   ngOnInit(): void {
-    this.newsService.getNewsArticles().subscribe(res => {
-      this.newsArticles = res.map(e => {
+    this.newsService.getNewsArticles().subscribe((res) => {
+      this.newsArticles = res.map((e) => {
         return {
           id: e.payload.doc.id,
-          ...(e.payload.doc.data() as object)
+          ...(e.payload.doc.data() as object),
         } as News;
       });
       this.newsArticles.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
     });
-
   }
 
   setThumbnail(files: any) {
@@ -63,10 +84,11 @@ Something...
 
   resetForm() {
     this.addNewsForm.reset();
-    this.previewThumbnailUrl = '';
-    this.updateNewsId = '';
-    this.mainButtonText = 'Add News Article';
-    this.markdown = '';
+    this.previewThumbnailUrl = "";
+    this.updateNewsId = "";
+    this.mainButtonText = "Add News Article";
+    this.markdown = "";
+    this.imageChangedEvent = null;
   }
 
   manageNews() {
@@ -76,41 +98,43 @@ Something...
       readMarkdown = this.markdown;
       readLink = null;
     } else {
-      readMarkdown = '';
+      readMarkdown = "";
       readLink = this.link;
     }
 
     // Update
-    if (this.mainButtonText.startsWith('Update')) {
+    if (this.mainButtonText.startsWith("Update")) {
       if (this.thumbnail === null) {
         const newNewsArticle = {
           id: this.updateNewsId,
-          name: this.addNewsForm.get('name').value,
-          date: this.addNewsForm.get('date').value,
+          name: this.addNewsForm.get("name").value,
+          date: this.addNewsForm.get("date").value,
           markdown: readMarkdown,
           link: readLink,
           thumbnail: null,
-          thumbnailUrl: this.previewThumbnailUrl
+          thumbnailUrl: this.previewThumbnailUrl,
         };
         this.newsService.updateNews(newNewsArticle);
       } else {
         const newsId = this.updateNewsId;
-        const newsName = this.addNewsForm.get('name').value;
-        const newsDate = this.addNewsForm.get('date').value;
-        this.uploadService.uploadFile(this.thumbnail, 'assets/thumnails/').then((snapshot) => {
-          snapshot.ref.getDownloadURL().then((downloadUrl) => {
-            const newNewsArticle = {
-              id: newsId,
-              name: newsName,
-              date: newsDate,
-              markdown: readMarkdown,
-              link: readLink,
-              thumbnail: null,
-              thumbnailUrl: downloadUrl
-            };
-            this.newsService.updateNews(newNewsArticle);
+        const newsName = this.addNewsForm.get("name").value;
+        const newsDate = this.addNewsForm.get("date").value;
+        this.uploadService
+          .uploadFile(this.thumbnail, "assets/thumnails/")
+          .then((snapshot) => {
+            snapshot.ref.getDownloadURL().then((downloadUrl) => {
+              const newNewsArticle = {
+                id: newsId,
+                name: newsName,
+                date: newsDate,
+                markdown: readMarkdown,
+                link: readLink,
+                thumbnail: null,
+                thumbnailUrl: downloadUrl,
+              };
+              this.newsService.updateNews(newNewsArticle);
+            });
           });
-        });
       }
       this.resetForm();
       return;
@@ -118,35 +142,37 @@ Something...
 
     // Add
     if (this.thumbnail === null) {
-      const newsName = this.addNewsForm.get('name').value;
+      const newsName = this.addNewsForm.get("name").value;
       const newNewsArticle = {
         name: newsName,
-        date: this.addNewsForm.get('date').value,
+        date: this.addNewsForm.get("date").value,
         markdown: readMarkdown,
         link: readLink,
         thumbnail: null,
-        thumbnailUrl: ''
+        thumbnailUrl: "",
       };
       console.log(newNewsArticle);
       this.newsService.addNews(newNewsArticle);
       this.resetForm();
     } else {
-      const newsName = this.addNewsForm.get('name').value;
-      const newsDate = this.addNewsForm.get('date').value;
-      this.uploadService.uploadFile(this.thumbnail, 'assets/thumnails/').then((snapshot) => {
-        snapshot.ref.getDownloadURL().then((downloadUrl) => {
-          const newNewsArticle = {
-            name: newsName,
-            date: newsDate,
-            markdown: readMarkdown,
-            link: readLink,
-            thumbnail: null,
-            thumbnailUrl: downloadUrl
-          };
-          console.log(newNewsArticle);
-          this.newsService.addNews(newNewsArticle);
+      const newsName = this.addNewsForm.get("name").value;
+      const newsDate = this.addNewsForm.get("date").value;
+      this.uploadService
+        .uploadFile(this.thumbnail, "assets/thumnails/")
+        .then((snapshot) => {
+          snapshot.ref.getDownloadURL().then((downloadUrl) => {
+            const newNewsArticle = {
+              name: newsName,
+              date: newsDate,
+              markdown: readMarkdown,
+              link: readLink,
+              thumbnail: null,
+              thumbnailUrl: downloadUrl,
+            };
+            console.log(newNewsArticle);
+            this.newsService.addNews(newNewsArticle);
+          });
         });
-      });
       this.resetForm();
     }
   }
@@ -157,15 +183,15 @@ Something...
 
   setupNewsUpdate(news: News) {
     this.updateNewsId = news.id;
-    this.addNewsForm.get('name').setValue(news.name);
-    this.addNewsForm.get('date').setValue(news.date);
+    this.addNewsForm.get("name").setValue(news.name);
+    this.addNewsForm.get("date").setValue(news.date);
     this.markdown = news.markdown;
     this.link = news.link;
     this.previewThumbnailUrl = news.thumbnailUrl;
     this.thumbnail = null;
-    this.mainButtonText = 'Update News Article';
+    this.mainButtonText = "Update News Article";
 
-    if (this.link !== null ) {
+    if (this.link !== null) {
       this.isLinkOnlyFormat = true;
     } else {
       this.isLinkOnlyFormat = false;

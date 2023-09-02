b0c931e052f81f9707f3a15ebbddc54c3ac93065
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-02 14:18:33 -0600

    changed pwd req to 6 chars (firebase req)

--- src/app/portal/register/register.component.ts
+++ src/app/portal/register/register.component.ts
@@ -1,43 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent {
   signUpForm: UntypedFormGroup;
   passwordsDoNotMatch: boolean;
   ucalgaryEmailUsed = true;
 
-  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService) {
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private authService: AuthService
+  ) {
     this.signUpForm = this.formBuilder.group({
-      displayName: [''],
-      email: ['', Validators.pattern('.+@.+\.c.+')],
-      password: ['', Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!#%*?&])[A-Za-z\d$@$!%*?&].{8,}')],
-      passwordConfirm: [''],
+      displayName: [""],
+      email: ["", Validators.pattern(".+@.+.c.+")],
+      password: ["", Validators.pattern("^.{6,}$")],
+      passwordConfirm: [""],
     });
   }
 
   SignUp() {
-    if (this.signUpForm.get('password').value !== this.signUpForm.get('passwordConfirm').value) {
+    if (
+      this.signUpForm.get("password").value !==
+      this.signUpForm.get("passwordConfirm").value
+    ) {
       this.passwordsDoNotMatch = true;
       return;
     }
-    if (!this.signUpForm.get('email').value.includes('ucalgary.ca')) {
+    if (!this.signUpForm.get("email").value.includes("ucalgary.ca")) {
       this.ucalgaryEmailUsed = false;
       return;
     }
     this.passwordsDoNotMatch = false;
-    this.authService.SignUp(this.signUpForm.get('displayName').value,
-                            this.signUpForm.get('email').value,
-                            this.signUpForm.get('password').value);
+    this.authService.SignUp(
+      this.signUpForm.get("displayName").value,
+      this.signUpForm.get("email").value,
+      this.signUpForm.get("password").value
+    );
   }
 
   SignUpSuccess() {
-    const user = JSON.parse(window.sessionStorage.getItem('User'));
+    const user = JSON.parse(window.sessionStorage.getItem("User"));
     return user !== null && !user.verified;
   }
 }

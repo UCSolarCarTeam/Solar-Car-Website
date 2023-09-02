409839b237c3f5a2573f61b4cc2cd219ac2c3e67
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-02 14:19:29 -0600

    reset button now shows message

--- src/app/portal/reset-password/reset-password.component.ts
+++ src/app/portal/reset-password/reset-password.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-reset-password',
-  templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  selector: "app-reset-password",
+  templateUrl: "./reset-password.component.html",
+  styleUrls: ["./reset-password.component.css"],
 })
 export class ResetPasswordComponent {
+  isMessageVisible = false;
+  message = "Please check your email to reset your password";
+  showMessage() {
+    this.isMessageVisible = true;
+  }
 
   resetForm: UntypedFormGroup;
 
-  constructor(private authService: AuthService, private formBuilder: UntypedFormBuilder) {
-    this.resetForm =  this.formBuilder.group({
-      email: ['', Validators.pattern('.+@.+\..+')]
+  constructor(
+    private authService: AuthService,
+    private formBuilder: UntypedFormBuilder
+  ) {
+    this.resetForm = this.formBuilder.group({
+      email: ["", Validators.pattern(".+@.+..+")],
     });
   }
 
   resetPasswordSubmit() {
-    this.authService.ResetPassword(this.resetForm.get('email').value);
+    this.authService.ResetPassword(this.resetForm.get("email").value);
   }
 }

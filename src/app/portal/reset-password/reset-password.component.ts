d27e3a74322283d9b1a2922a45ecadb00879c9d8
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-23 11:13:01 -0600

    notifications for password reset

--- src/app/portal/reset-password/reset-password.component.ts
+++ src/app/portal/reset-password/reset-password.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import AWN from "awesome-notifications";
+let globalOptions = {};
+let notifier = new AWN(globalOptions);
+let nextCallOptions = {};
 @Component({
-  selector: 'app-reset-password',
-  templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  selector: "app-reset-password",
+  templateUrl: "./reset-password.component.html",
+  styleUrls: ["./reset-password.component.css"],
 })
 export class ResetPasswordComponent {
-
   resetForm: UntypedFormGroup;
-
-  constructor(private authService: AuthService, private formBuilder: UntypedFormBuilder) {
-    this.resetForm =  this.formBuilder.group({
-      email: ['', Validators.pattern('.+@.+\..+')]
+  resetPasswordNotification(promise: Promise<any>) {
+    notifier.async(
+      promise,
+      "Password has been reset",
+      "Password has failed to reset. Please contact tech support"
+    );
+  }
+  constructor(
+    private authService: AuthService,
+    private formBuilder: UntypedFormBuilder
+  ) {
+    this.resetForm = this.formBuilder.group({
+      email: ["", Validators.pattern(".+@.+..+")],
     });
   }
 
   resetPasswordSubmit() {
-    this.authService.ResetPassword(this.resetForm.get('email').value);
+    let promise = this.authService.ResetPassword(
+      this.resetForm.get("email").value
+    );
+    this.resetPasswordNotification(promise);
   }
 }
--- src/app/services/file-delete.service.ts
+++ src/app/services/file-delete.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { UploadTask } from '@angular/fire/compat/storage/interfaces';
+import { Injectable } from "@angular/core";
+import { AngularFireStorage } from "@angular/fire/compat/storage";
+import { UploadTask } from "@angular/fire/compat/storage/interfaces";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FileDeleteService {
-
-  constructor(private storage: AngularFireStorage) { }
+  constructor(private storage: AngularFireStorage) {}
 
   deleteFile(path: string) {
     const storageRef = this.storage.storage.refFromURL(path);
-    return storageRef.delete();
+    return storageRef.delete().catch((err) => {
+      throw err;
+    });
   }
 }

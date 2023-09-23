ca93e0c265247471e5e181525cb43ac40d0589a3
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-23 10:48:51 -0600

    add notifications to registration

--- src/app/portal/register/register.component.ts
+++ src/app/portal/register/register.component.ts
@@ -1,43 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import AWN from "awesome-notifications";
+let globalOptions = {};
+let notifier = new AWN(globalOptions);
+let nextCallOptions = {};
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent {
   signUpForm: UntypedFormGroup;
   passwordsDoNotMatch: boolean;
   ucalgaryEmailUsed = true;
-
-  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService) {
+  registerUserNotification(promise: Promise<any>) {
+    notifier.async(
+      promise,
+      "User has been registered",
+      "User has failed to register, contact tech support"
+    );
+  }
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private authService: AuthService
+  ) {
     this.signUpForm = this.formBuilder.group({
-      displayName: [''],
-      email: ['', Validators.pattern('.+@.+\.c.+')],
-      password: ['', Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!#%*?&])[A-Za-z\d$@$!%*?&].{8,}')],
-      passwordConfirm: [''],
+      displayName: [""],
+      email: ["", Validators.pattern(".+@.+.c.+")],
+      password: [
+        "",
+        Validators.pattern(
+          "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!#%*?&])[A-Za-zd$@$!%*?&].{8,}"
+        ),
+      ],
+      passwordConfirm: [""],
     });
   }
 
   SignUp() {
-    if (this.signUpForm.get('password').value !== this.signUpForm.get('passwordConfirm').value) {
+    if (
+      this.signUpForm.get("password").value !==
+      this.signUpForm.get("passwordConfirm").value
+    ) {
       this.passwordsDoNotMatch = true;
       return;
     }
-    if (!this.signUpForm.get('email').value.includes('ucalgary.ca')) {
+    if (!this.signUpForm.get("email").value.includes("ucalgary.ca")) {
       this.ucalgaryEmailUsed = false;
       return;
     }
     this.passwordsDoNotMatch = false;
-    this.authService.SignUp(this.signUpForm.get('displayName').value,
-                            this.signUpForm.get('email').value,
-                            this.signUpForm.get('password').value);
+    let promise = this.authService.SignUp(
+      this.signUpForm.get("displayName").value,
+      this.signUpForm.get("email").value,
+      this.signUpForm.get("password").value
+    );
+    this.registerUserNotification(promise);
   }
 
   SignUpSuccess() {
-    const user = JSON.parse(window.sessionStorage.getItem('User'));
+    const user = JSON.parse(window.sessionStorage.getItem("User"));
     return user !== null && !user.verified;
   }
 }

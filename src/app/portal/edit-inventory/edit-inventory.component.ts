0f6201bcc83e26e83921ef418b3785cf7838b4fd
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-01-13 14:36:55 -0700

    fixed add item image bug

--- src/app/portal/edit-inventory/edit-inventory.component.ts
+++ src/app/portal/edit-inventory/edit-inventory.component.ts
@@ -283,7 +283,7 @@ export class EditInventoryComponent implements OnInit {
 
   manageItemForm() {
     if (this.submitButtonText.startsWith("Edit Item")) {
-      if (this.image === null) {
+      if (this.image === null || this.image === undefined) {
         const newItem = {
           id: this.updateItemId,
           name: this.addItemForm.get("name").value,
@@ -355,7 +355,7 @@ export class EditInventoryComponent implements OnInit {
       return;
     }
     // Adding New Item
-    if (this.image === null) {
+    if (this.image === null || this.image === undefined) {
       const newItem = {
         name: this.addItemForm.get("name").value,
         type: this.addItemForm.get("type").value,
